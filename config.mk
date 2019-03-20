35f03c27694ef4073330015d113a725290edf1e4
niplav <niplav@posteo.net>
2019-03-20 14:50:51 +0100

    Added sceletons for articles.

---
+++ config.mk
@@ -0,0 +1,13 @@
+MARKDOWN=markdown_py
+
+CONF=config.mk
+
+HTML=index.html \
+	about.html \
+	changelog.html \
+	differences_gnu_and_posix_make.html \
+	end_of_discussion.html \
+	pick_up.html \
+	preference_frustration_auctions.html \
+	two_people_on_a_planet.html \
+	uses_this.html \
