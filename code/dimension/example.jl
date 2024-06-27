1ced81a67cbc3917f8c0208f6b463a5d99785d3c
niplav <niplav@posteo.net>
2024-06-27 21:36:37 +0200

    Little stuff.

--- code/dimension/example.jl
+++ code/dimension/example.jl
@@ -3,9 +3,10 @@ using TSne, UMAP, Plots
 data=rand(10000, 2000)
 
 reduced=tsne(data)
+reduced_moredims=tsne(transpose(data))
 reduced_5=tsne(data, 2, 0, 1000, 5.0)
 
-reduced_umap=umap(transpose(data))
+#reduced_umap=umap(transpose(data))
 
 scatter(reduced[:,1],reduced[:,2])
 scatter(reduced_umap[1,:],reduced_umap[2,:])
